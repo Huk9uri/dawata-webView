d82f8923694bcf65bdfa127cc84d6f4de8330247
Huk9uri <gurrb19@naver.com>
2025-02-20 03:27:19 +0900

    feat : ControlBar props 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 import {
   ControlBar,
@@ -7,29 +7,29 @@ import {
   ParticipantTile,
   RoomAudioRenderer,
   useTracks,
-} from '@livekit/components-react';
+} from "@livekit/components-react";
 
-import '@livekit/components-styles';
+import "@livekit/components-styles";
 
-import { Track } from 'livekit-client';
+import { Track } from "livekit-client";
 
-const serverUrl = 'wss://dawata-3r907ysh.livekit.cloud';
+const serverUrl = "wss://dawata-3r907ysh.livekit.cloud";
 
 export default function App() {
   const [token, setToken] = useState<string | null>(null);
-  const [username, setUsername] = useState('');
-  const [room, setRoom] = useState('');
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
 
   const handleJoin = async () => {
     try {
-      const response = await fetch('https://i12a301.p.ssafy.io/live/1/token');
+      const response = await fetch("https://i12a301.p.ssafy.io/live/1/token");
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       const data = await response.json();
       setToken(data.data);
     } catch (error) {
-      console.error('Failed to fetch token:', error);
+      console.error("Failed to fetch token:", error);
     }
   };
 
@@ -54,7 +54,6 @@ export default function App() {
     );
   }
 
-
   return (
     <LiveKitRoom
       video={true}
@@ -63,7 +62,7 @@ export default function App() {
       serverUrl={serverUrl}
       // Use the default LiveKit theme for nice styles.
       data-lk-theme="default"
-      style={{ height: '100vh' }}
+      style={{ height: "100vh" }}
     >
       {/* Your custom component with basic video conferencing functionality. */}
       <MyVideoConference />
@@ -71,7 +70,16 @@ export default function App() {
       <RoomAudioRenderer />
       {/* Controls for the user to start/stop audio, video, and screen
       share tracks and to leave the room. */}
-      <ControlBar />
+      <ControlBar
+        controls={{
+          microphone: true,
+          camera: true,
+          chat: true,
+          screenShare: true,
+          leave: true,
+          settings: true,
+        }}
+      />
     </LiveKitRoom>
   );
 }
@@ -84,10 +92,13 @@ function MyVideoConference() {
       { source: Track.Source.Camera, withPlaceholder: true },
       { source: Track.Source.ScreenShare, withPlaceholder: false },
     ],
-    { onlySubscribed: false },
+    { onlySubscribed: false }
   );
   return (
-    <GridLayout tracks={tracks} style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
+    <GridLayout
+      tracks={tracks}
+      style={{ height: "calc(100vh - var(--lk-control-bar-height))" }}
+    >
       {/* The GridLayout accepts zero or one child. The child is used
       as a template to render all passed in tracks. */}
       <ParticipantTile />
